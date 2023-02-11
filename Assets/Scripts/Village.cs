e50a232be30571c90f404b36f629ae5b7350efdd
Dennis Plotnik <dennis@flexa.co>
2023-02-10 23:12:58 -0500

    Allocate villager growth to idols

--- Assets/Scripts/Village.cs
+++ Assets/Scripts/Village.cs
@@ -1,35 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Village : MonoBehaviour
 {
-  private const int VILLAGER_GROWTH_PT = 5;
-  private TurnController turnController;
-  private int previousTurn;
-  public VillageInfo villageInfoPrefab;
-  private VillageInfo villageInfo;
-  public GameObject sideMenu;
-  public VillageStats stats;
-  private float timer;
+    private const int VILLAGER_GROWTH_PT = 10;
+    private const int IDOLPOOL_VILLAGER_GROWTH_PT = 5;
+    private const int IDOLPOOL_VILLAGER_TARGET = 50;
 
-  void Start()
-  {
-    turnController = GameObject.FindObjectOfType<TurnController>();
+    private TurnController turnController;
+    private int previousTurn;
+    public VillageInfo villageInfoPrefab;
+    private VillageInfo villageInfo;
+    public GameObject sideMenu;
+    private VillageStats idolPool;
+    private float timer;
 
-    stats = new VillageStats();
-    villageInfo = Instantiate(villageInfoPrefab, sideMenu.transform);
-    villageInfo.villageStats = stats;
-  }
-
-  void Update()
-  {
-    if (turnController.turn != previousTurn)
+    void Start()
     {
-      stats.villagers += VILLAGER_GROWTH_PT * (turnController.turn - previousTurn);
-      previousTurn = turnController.turn;
+        turnController = GameObject.FindObjectOfType<TurnController>();
 
+        villageInfo = Instantiate(villageInfoPrefab, sideMenu.transform);
+        villageInfo.villageStats = new VillageInfoProps();
     }
-  }
+
+    void Update()
+    {
+        if (turnController.turn != previousTurn)
+        {
+            updateStats(turnController.turn - previousTurn);
+
+            previousTurn = turnController.turn;
+        }
+    }
+
+    void updateStats(int dt)
+    {
+        Debug.Log("DP updateStats");
+        int newVillagers = VILLAGER_GROWTH_PT * dt;
+        Debug.Log(newVillagers);
+        if (villageInfo.idolToggle.isOn)
+        {
+            villageInfo.villageStats.idolPool.villagers += (newVillagers -= IDOLPOOL_VILLAGER_GROWTH_PT * dt);
+            Debug.Log(newVillagers);
+            if (villageInfo.villageStats.idolPool.villagers >= IDOLPOOL_VILLAGER_TARGET)
+            {
+                villageInfo.villageStats.idolPool.villagers -= IDOLPOOL_VILLAGER_TARGET;
+                villageInfo.villageStats.mainPool.idols += 1;
+            }
+        }
+        villageInfo.villageStats.mainPool.villagers += newVillagers;
+        Debug.Log(newVillagers);
+    }
 }
--- Assets/Scripts/VillageInfo.cs
+++ Assets/Scripts/VillageInfo.cs
@@ -2,32 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class VillageStats
 {
-  public int villagers;
-  public int idols;
+    public int villagers;
+    public int idols;
+}
+
+[System.Serializable]
+public class VillageInfoProps
+{
+    public VillageInfoProps()
+    {
+        mainPool = new VillageStats();
+        idolPool = new VillageStats();
+    }
+    public VillageStats mainPool;
+    public VillageStats idolPool;
 }
 
 public class VillageInfo : MonoBehaviour
 {
-  public VillageStats villageStats;
-  private VillageStats oldVillageStats;
-  [SerializeField] private TextMeshProUGUI villagersText;
-  [SerializeField] private TextMeshProUGUI idolsText;
+    public VillageInfoProps villageStats;
+    private VillageInfoProps oldVillageStats;
+    [SerializeField] private TextMeshProUGUI villagersText;
+    [SerializeField] private TextMeshProUGUI idolsText;
+    [SerializeField] public Toggle idolToggle;
 
-  void Start()
-  {
-
-  }
-
-  void Update()
-  {
-    if (villageStats != oldVillageStats)
+    void Start()
     {
-      villagersText.text = "Villagers: " + villageStats.villagers;
-      idolsText.text = "Idols: " + villageStats.idols;
     }
-  }
+
+    void Update()
+    {
+        if (villageStats != oldVillageStats)
+        {
+            villagersText.text = "Villagers: " + villageStats.mainPool.villagers;
+            idolsText.text = "Idols: " + villageStats.mainPool.idols;
+            if (villageStats.idolPool.villagers > 0)
+            {
+                idolsText.text += " [" + villageStats.idolPool.villagers + " / 50]";
+            }
+        }
+    }
 }
